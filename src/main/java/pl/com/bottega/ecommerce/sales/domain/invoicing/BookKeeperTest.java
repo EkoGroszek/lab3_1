95f9ec64bfee65725089126b5b98a43dd6966933
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-04-26 15:30:58 +0200

    isolation TestCase5 added

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -61,6 +61,15 @@ public class BookKeeperTest {
         Assert.assertThat(invoice.getItems().size(), is(0));
     }
 
+    @Test public void noRequestItemShouldReturnInvoiceShouldreturnInvoiceWithNetEqualsZero() {
+        when(taxPolicy.calculateTax(any(), any())).thenReturn(tax);
+        BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        Assert.assertThat(invoice.getNet(), is(Money.ZERO));
+    }
+
     @Test public void twoRequestItemsShouldCallCalculateTaxTwice() {
         when(taxPolicy.calculateTax(any(), any())).thenReturn(tax);
         BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
